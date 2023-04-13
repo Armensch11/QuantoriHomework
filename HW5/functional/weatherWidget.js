92a4038881065f1e876764715237a2c1e8fd10a2
Armensch11 <armenschk@gmail.com>
2023-04-13 17:16:28 +0400

    weather widget done for default location

--- HW5/functional/weatherWidget.js
+++ HW5/functional/weatherWidget.js
@@ -1,36 +1,55 @@
 export async function weatherWidget() {
-  const apiKey = "8865504129134593933100446231304";
+  navigator.geolocation.getCurrentPosition(locationSuccess, locationError);
+}
+
+function widgetFrame(icon, temp, name) {
   const container = document.getElementsByClassName("header")[0];
   const weatherContainer = document.createElement("div");
   weatherContainer.setAttribute("class", "weather-wrapper");
   container.appendChild(weatherContainer);
-  const locationDefault = "Tbilisi";
+  const image = document.createElement("img");
+  image.setAttribute("class", "weather-wrapper__image");
+  image.src = icon;
+  const temperature = document.createElement("span");
+  temperature.setAttribute("class", "weather-wrapper__temperature");
+  temperature.innerText = temp;
+  const city = document.createElement("span");
+  city.setAttribute("class", "weather-wrapper__city");
+  city.innerText = name;
+  [image, temperature, city].forEach((el) => weatherContainer?.appendChild(el));
+}
+
+const locationSuccess = async (pos) => {
+  const apiKey = "8865504129134593933100446231304";
   const locationCurrent = {};
   let reqParam = "";
   let weather = {};
-
-  navigator.geolocation.getCurrentPosition(async (pos) => {
-    locationCurrent.latitude = pos.coords.latitude.toFixed(4);
-    locationCurrent.longitude = pos.coords.longitude.toFixed(4);
-    reqParam = locationCurrent.latitude + "," + locationCurrent.longitude;
-    const weatherData = await fetch(
-      `http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${
-        reqParam.length ? reqParam : locationDefault
-      }&aqi=no`
-    );
-    weather = await weatherData.json();
-    console.log(weather);
-    const image = document.createElement("img");
-    image.setAttribute("class", "weather-wrapper__image");
-    image.src = weather.current.condition.icon;
-    const temperature = document.createElement("span");
-    temperature.setAttribute("class", "weather-wrapper__temperature");
-    temperature.innerText = weather.current.temp_c;
-    const city = document.createElement("span");
-    city.setAttribute("class", "weather-wrapper__city");
-    city.innerText = weather.location.name;
-    [image, temperature, city].forEach((el) =>
-      weatherContainer?.appendChild(el)
-    );
-  });
-}
+  locationCurrent.latitude = pos.coords.latitude.toFixed(4);
+  locationCurrent.longitude = pos.coords.longitude.toFixed(4);
+  reqParam = locationCurrent.latitude + "," + locationCurrent.longitude;
+  const weatherData = await fetch(
+    `http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${reqParam}&aqi=no`
+  );
+  weather = await weatherData.json();
+  console.log(weather);
+  const [icon, temp, city] = [
+    weather.current.condition.icon,
+    weather.current.temp_c,
+    weather.location.name,
+  ];
+  widgetFrame(icon, temp, city);
+};
+const locationError = async (pos) => {
+  const apiKey = "8865504129134593933100446231304";
+  const locationDefault = "Tbilisi";
+  const weatherData = await fetch(
+    `http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${locationDefault}&aqi=no`
+  );
+  const weather = await weatherData.json();
+  const [icon, temp, city] = [
+    weather.current.condition.icon,
+    weather.current.temp_c,
+    weather.location.name,
+  ];
+  widgetFrame(icon, temp, city);
+};
