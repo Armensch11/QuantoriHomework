59e2ab106b88a9eaebbd128ef09d500ff9a002f9
Armensch11 <armenschk@gmail.com>
2023-04-15 11:22:54 +0400

    experimenting with popup ver.2

--- HW5/functional/popup.js
+++ HW5/functional/popup.js
@@ -1,9 +1,9 @@
 export function popup(message, type = "success") {
   const body = document.getElementsByTagName("body")[0];
   const popContainer = document.createElement("div");
-  popContainer.setAttribute("class", "fetch-popup");
-  popContainer.style.width = "240px";
-  popContainer.style.height = "60px";
+  popContainer.setAttribute("class", "popup");
+  popContainer.style.width = "160px";
+  popContainer.style.height = "40px";
   popContainer.backgroundColor = "#e6e3e3";
   if (type === "success") {
     popContainer.style.border = "1px solid #67cf92";
@@ -16,7 +16,7 @@ export function popup(message, type = "success") {
   popContainer.innerText = message;
   popContainer.style.zIndex = "100";
   popContainer.style.position = "absolute";
-  popContainer.style.top = "calc(100% - 30px)";
+  popContainer.style.top = "calc(100% - 20px)";
   popContainer.style.left = "80%";
   popContainer.style.display = "block";
   popContainer.style.transform = "translate(-50%, -50%)";
